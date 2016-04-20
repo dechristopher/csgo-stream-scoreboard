dd298b02277a21315914e9041752852352a7e08c
Andrew DeChristropher <dechristophera@wit.edu>
2016-04-20 17:58:45 -0400

    Initial commit

---
+++ CSGO-Stream-Scoreboard/CSGO-Stream-Scoreboard/UI.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace CSGOSS
+{
+    partial class UI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UI));
+            this.kLabel = new System.Windows.Forms.Label();
+            this.aLabel = new System.Windows.Forms.Label();
+            this.dLabel = new System.Windows.Forms.Label();
+            this.kNum = new System.Windows.Forms.Label();
+            this.dNum = new System.Windows.Forms.Label();
+            this.aNum = new System.Windows.Forms.Label();
+            this.kThisRound = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // kLabel
+            // 
+            this.kLabel.AutoSize = true;
+            this.kLabel.Font = new System.Drawing.Font("Headline One", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kLabel.ForeColor = System.Drawing.Color.Silver;
+            this.kLabel.Location = new System.Drawing.Point(1, 5);
+            this.kLabel.Name = "kLabel";
+            this.kLabel.Size = new System.Drawing.Size(24, 29);
+            this.kLabel.TabIndex = 4;
+            this.kLabel.Text = "K";
+            this.kLabel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UI_RClick);
+            // 
+            // aLabel
+            // 
+            this.aLabel.AutoSize = true;
+            this.aLabel.Font = new System.Drawing.Font("Headline One", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aLabel.ForeColor = System.Drawing.Color.Silver;
+            this.aLabel.Location = new System.Drawing.Point(180, 5);
+            this.aLabel.Name = "aLabel";
+            this.aLabel.Size = new System.Drawing.Size(26, 29);
+            this.aLabel.TabIndex = 8;
+            this.aLabel.Text = "A";
+            this.aLabel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UI_RClick);
+            // 
+            // dLabel
+            // 
+            this.dLabel.AutoSize = true;
+            this.dLabel.Font = new System.Drawing.Font("Headline One", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dLabel.ForeColor = System.Drawing.Color.Silver;
+            this.dLabel.Location = new System.Drawing.Point(111, 5);
+            this.dLabel.Name = "dLabel";
+            this.dLabel.Size = new System.Drawing.Size(24, 29);
+            this.dLabel.TabIndex = 9;
+            this.dLabel.Text = "D";
+            this.dLabel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UI_RClick);
+            // 
+            // kNum
+            // 
+            this.kNum.AutoSize = true;
+            this.kNum.Font = new System.Drawing.Font("Headline One", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kNum.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.kNum.Location = new System.Drawing.Point(59, 5);
+            this.kNum.Name = "kNum";
+            this.kNum.Size = new System.Drawing.Size(30, 37);
+            this.kNum.TabIndex = 10;
+            this.kNum.Text = "0";
+            this.kNum.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UI_RClick);
+            // 
+            // dNum
+            // 
+            this.dNum.AutoSize = true;
+            this.dNum.Font = new System.Drawing.Font("Headline One", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dNum.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.dNum.Location = new System.Drawing.Point(127, 5);
+            this.dNum.Name = "dNum";
+            this.dNum.Size = new System.Drawing.Size(30, 37);
+            this.dNum.TabIndex = 11;
+            this.dNum.Text = "0";
+            this.dNum.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UI_RClick);
+            // 
+            // aNum
+            // 
+            this.aNum.AutoSize = true;
+            this.aNum.Font = new System.Drawing.Font("Headline One", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aNum.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.aNum.Location = new System.Drawing.Point(198, 5);
+            this.aNum.Name = "aNum";
+            this.aNum.Size = new System.Drawing.Size(30, 37);
+            this.aNum.TabIndex = 12;
+            this.aNum.Text = "0";
+            this.aNum.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UI_RClick);
+            // 
+            // kThisRound
+            // 
+            this.kThisRound.AutoSize = true;
+            this.kThisRound.Font = new System.Drawing.Font("Headline One", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kThisRound.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.kThisRound.Location = new System.Drawing.Point(17, 5);
+            this.kThisRound.Name = "kThisRound";
+            this.kThisRound.Size = new System.Drawing.Size(47, 37);
+            this.kThisRound.TabIndex = 13;
+            this.kThisRound.Text = "0 /";
+            this.kThisRound.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UI_RClick);
+            // 
+            // UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(240, 43);
+            this.Controls.Add(this.kThisRound);
+            this.Controls.Add(this.aNum);
+            this.Controls.Add(this.dNum);
+            this.Controls.Add(this.kNum);
+            this.Controls.Add(this.dLabel);
+            this.Controls.Add(this.aLabel);
+            this.Controls.Add(this.kLabel);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stream Scoreboard";
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UI_RClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label kLabel;
+        private System.Windows.Forms.Label aLabel;
+        private System.Windows.Forms.Label dLabel;
+        private System.Windows.Forms.Label kNum;
+        private System.Windows.Forms.Label dNum;
+        private System.Windows.Forms.Label aNum;
+        private System.Windows.Forms.Label kThisRound;
+    }
+}
+
---
+++ CSGO-Stream-Scoreboard/CSGO-Stream-Scoreboard/UI.cs
@@ -0,0 +1,154 @@
+﻿using CSGSI;
+using CSGSI.Nodes;
+using SteamKit2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSGOSS
+{
+    public partial class UI : Form
+    {
+        static string[] settings;
+        static GameStateListener gsl;
+        static string sid;
+        static SteamID s;
+
+        public UI()
+        {
+            InitializeComponent();
+            this.FormClosed += new FormClosedEventHandler(stopGSListen);
+
+            settings = grabSid();
+
+            if (settings.Length.Equals(0))
+            {
+                MessageBox.Show("Please paste your SteamID into the steamid.txt file created in the folder this program is in and restart the program.");
+                Environment.Exit(0);
+            }
+            else
+            {
+                sid = settings[0];
+
+                //Set custom colors
+                if (settings.Length > 1)
+                {
+                    if (settings.Length.Equals(7))
+                    {
+                        string u, v, w, x, y, z;
+
+                        u = settings[1];
+                        v = settings[2];
+                        w = settings[3];
+                        x = settings[4];
+                        y = settings[5];
+                        z = settings[6];
+
+                        setColors(u, v, w, x, y, z);
+                    }
+                    else
+                    {
+                        MessageBox.Show("In order to use custom colors, you muse serparate two sets of RGB values in the config.txt file like so:\n\n" +
+                            "<steamid> <R> <G> <B> <R> <G> <B>\n\n" +
+                            "All values must be within 0-255 and must be separated by spaces. The first set of values are for background color and the second set are for stat counter color. En example configuration would look like the following:\n\n" +
+                            "STEAM_0:0:1234 64 64 64 0 192 0");
+                    }
+                }
+
+                s = new SteamID(sid.Trim());
+
+                if (s.IsValid)
+                {
+                    gsl = new GameStateListener(3000);
+                    gsl.NewGameState += new NewGameStateHandler(OnNewGameState);
+                    if (!gsl.Start())
+                    {
+                        Environment.Exit(0);
+                    }
+                    Console.WriteLine("Listening...");
+                }
+                else
+                {
+                    MessageBox.Show("INVALID STEAMID PROVIDED\n\nSteamID must be in regular SteamID format. EX: STEAM_0:0:39990");
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+
+
+        private void stopGSListen(object sender, FormClosedEventArgs e)
+        {
+            if (gsl != null) { gsl.Stop(); }
+            Environment.Exit(0);
+        }
+
+        void OnNewGameState(GameState gs)
+        {
+            if (gs.Player.SteamID.Equals(s.ConvertToUInt64().ToString()))
+            {
+                if (!gs.Player.MatchStats.Kills.ToString().Equals("-1")) { this.kNum.Text = gs.Player.MatchStats.Kills.ToString(); }
+                if (!gs.Player.MatchStats.Deaths.ToString().Equals("-1")) { this.dNum.Text = gs.Player.MatchStats.Deaths.ToString(); }
+                if (!gs.Player.MatchStats.Assists.ToString().Equals("-1")) { this.aNum.Text = gs.Player.MatchStats.Assists.ToString(); }
+                if (!gs.Player.State.RoundKills.ToString().Equals("-1")) { this.kThisRound.Text = gs.Player.State.RoundKills.ToString() + " /"; }
+
+                if (gs.Round.Phase.Equals("freezetime"))
+                {
+                    this.kThisRound.Text = "0 /";
+                }
+            }
+        }
+
+        string[] grabSid()
+        {
+            if (File.Exists(@"settings.txt"))
+            {
+                return System.IO.File.ReadAllText(@"settings.txt").Split(' ');
+            }
+            else
+            {
+                string def = "<SteamID> 64 64 64 0 192 0";
+                File.WriteAllText(@"settings.txt", def);
+                MessageBox.Show("A default settings file called \"settings.txt\" has been created. Please open it and replace <SteamID> with your SteamID 32 which can be found at http://steamid.co");
+                Environment.Exit(0);
+                return def.Split(' ');
+            }
+        }
+
+        private void setColors(string u, string v, string w, string x, string y, string z)
+        {
+            //Background color RGB values
+            int br = int.Parse(u);
+            int bg = int.Parse(v);
+            int bb = int.Parse(w);
+
+            //Number color RGB values
+            int r = int.Parse(x);
+            int g = int.Parse(y);
+            int b = int.Parse(z);
+
+            this.BackColor = Color.FromArgb(br, bg, bb);
+
+            kThisRound.ForeColor = Color.FromArgb(r, g, b);
+            kNum.ForeColor = Color.FromArgb(r, g, b);
+            dNum.ForeColor = Color.FromArgb(r, g, b);
+            aNum.ForeColor = Color.FromArgb(r, g, b);
+        }
+
+        private void UI_RClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                MessageBox.Show("This piece of software is free and open-sourced for all public and private use. It is licensed under the MIT license and all attribution must be given back to myself. I will not be held liable or responsible for any damages or issues caused by this software.\n\n" +
+                    "Originally created and maintained by:\n\nAndrew DeChristopher >> github.com/dechristopher\nandrewdechristopher@gmail.com", "About Stream Scoreboard");
+            }
+        }
+    }
+}
